0eaa60b45dd07c23b23707c0b49bfd33cd1bbcd8
Brice Fernandes <brice.fernandes@gmail.com>
2014-10-05 15:55:51 +0100

    Augment search data for fullview template

--- ext/src/home.js
+++ ext/src/home.js
@@ -1,3 +1,33 @@
+function zeroPad(num, size){ 
+  return ('000000000' + num).substr(-size); 
+}
+
+function format_date(date){
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  var hours  = (date.getHours() % 12).toString();
+  var minutes = date.getMinutes()
+
+  var datestring = ((hours == 0)?"12":hours) + ":" + zeroPad(minutes,2);
+  datestring += " " + (date.getHours()/12 > 1)?"pm":"am";
+  datestring += " " + days[date.getDay()];
+  datestring += " " + date.getDate();
+  datestring += " " + months[date.getMonth()];
+  datestring += " " + date.getFullYear(); 
+
+  return datestring;
+}
+
+function augment_data(data){
+
+  data.date = format_date(new Date(data.timestamp));
+  
+  data.percentage = Math.round(data.score * 100);
+
+  return data;
+}
+
 $(function(){
   console.log("Home.js");
   
@@ -16,9 +46,8 @@ $(function(){
       }
 
       data.personalScores = scores_to_data_template(results_to_scores(search));
-      //console.log(search);
       
-      var $search = $($.parseHTML(search_item_template(data)));
+      var $search = $($.parseHTML(search_item_template(augment_data(data))));
 
       $search.data("search", data);
       

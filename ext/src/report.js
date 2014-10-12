b256c1a062ff125ed1f50dc86dd847d8d7760f1e
Brice Fernandes <brice.fernandes@gmail.com>
2014-10-12 15:54:02 +0100

    Load spinner example

--- ext/src/report.js
+++ ext/src/report.js
@@ -40,6 +40,35 @@ var fetch_clusters = function(searches, onSuccess, onFailure){
   searches_to_categorised_results(searches, _onSuccess, _onFailure);
 };
 
+var spinners = [];
+var spin_all_the_things = function(selector){
+  var opts = {
+    lines: 2, // The number of lines to draw
+    length: 20, // The length of each line
+    width: 10, // The line thickness
+    radius: 0, // The radius of the inner circle
+    corners: 0, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: '#000', // #rgb or #rrggbb or array of colors
+    speed: 4, // Rounds per second
+    trail: 60, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: '50%', // Top position relative to parent
+    left: '50%' // Left position relative to parent
+  };
+  $(selector).each(function(i){
+    // this is where we would initialise the spinners on our page. 
+    // Because we're selecting by elements the spinner will need 
+    // need an element on the page that can retrieve using the selector
+    spinners.push(new Spinner(opts).spin(this));
+  })
+
+};
+
 /*
  * Page init
  */
@@ -536,6 +565,7 @@ var x = d3.scale.ordinal()
     return d;
   }
 
+  //$("#bar-chart-1").parent().find('.spinning').hide();
 }
 
 // $(function(){

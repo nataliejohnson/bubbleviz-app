0e78bc396d1881493a7afa83e5f64727a306700e
Brice Fernandes <brice.fernandes@gmail.com>
2014-10-19 19:16:19 +0100

    Code book building modifications

--- codebook/recipe.sh
+++ codebook/recipe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TEMPLATE="resources/notes-template.tex"
-
+BUILD="build"
 # 1: file
 # 2: title
 # 3: destination file
@@ -14,5 +14,31 @@ function mkpdf {
 
 	dos2unix < "${FILE}" \
 	| cat <(echo -e "---\ntitle: ${TITLE}\n---\n\`\`\`${LANG}") - <(echo -e '\n```\n') \
-	| pandoc -s --listings --latex-engine=xelatex --template="${TEMPLATE}" -f markdown -t latex -o "${DEST}" 
+	| pandoc -s --listings --latex-engine=xelatex --template="${TEMPLATE}" -f markdown -t latex -o "${BUILD}/${DEST}" 
 }
+
+for f in `cat ../manifest-exclusive.txt` ; do 
+	EXT="${f##*.}"
+
+	if [[ $EXT = "py" ]]; then
+		LANG="Python"
+	fi
+	if [[ $EXT == "html" ]]; then
+		LANG="HTML"
+	fi
+	if [[ $EXT == "css" ]]; then
+		LANG="CSS"
+	fi
+	if [[ $EXT == "sh" ]]; then
+		LANG="Shell"
+	fi
+	if [[ $EXT == "json" ]]; then
+		LANG="Javascript"
+	fi
+	if [[ $EXT == "js" ]]; then
+		LANG="Javascript"
+	fi
+	echo mkpdf "../$f" "$(basename $f)" "$(basename $f).pdf" $LANG;
+	mkpdf "../$f" "$(basename $f)" "$(basename $f).pdf" $LANG
+
+done
